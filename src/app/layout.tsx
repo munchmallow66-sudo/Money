3b118cc8b4bfea59087b4078a817e614c7b84083
MoligaX <munchmallow66@gmail.com>
2026-03-10 14:51:16 +0700

    fix: remove old svg icons to force browsers to use new png logo for favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,6 @@ export const metadata: Metadata = {
       { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/icon-384x384.png", sizes: "384x384", type: "image/png" },
       { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
-      { url: "/icons/icon.svg", type: "image/svg+xml" },
     ],
     apple: [
       { url: "/icons/icon-192x192.png", sizes: "192x192" },
